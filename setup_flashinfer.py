185a74217838d7352edf0cb803abe844d2d84af4
vinwerill <vincent.super8@gmail.com>
2026-07-02 18:54:08 +0800

    Add README.md

---
+++ setup_flashinfer.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+import os
+
+os.makedirs("flashinfer_test", exist_ok=True)
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+import os
+
+setup_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+setup(
+    name="flashinfer_test",
+
+    ext_modules=[
+        CUDAExtension(
+            name="flashinfer_test._HIP",
+
+            sources=[
+                "flashinfer.hip",
+            ],
+
+            include_dirs=[
+                os.path.join(
+                    setup_dir,
+                    "include_hip"
+                ),
+            ],
+
+            extra_compile_args={
+                "cxx": [
+                    "-O3",
+                ],
+
+                "hipcc": [
+                    "-O3",
+
+                    # 你的 GPU
+                    "--offload-arch=gfx1201",
+
+                    # HIPify後避免某些 CUDA header
+                    "-D__HIP_PLATFORM_AMD__",
+                ],
+            },
+        )
+    ],
+
+    cmdclass={
+        "build_ext": BuildExtension
+    }
+)
